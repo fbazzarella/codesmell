0e636d3e1a040f581ed2b4f94dcc740cdd27e852
Marcus Sá <marcus.m.sa@gmail.com>
2011-06-02 09:57:08 -0300

    Alterando configurações do projeto para carregar I18n pt-BR por padrão

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+	
+	before_filter :set_locale 
+		
+	def set_locale 		
+		I18n.locale = params[:locale] 
+	end  
 end
