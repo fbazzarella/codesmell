48615540b365c0d693a55bbcfc67cd39657fe2b3
Felipe Bazzarella <fbazzarella@gmail.com>
2011-06-03 11:49:39 -0300

    Exibe mensagem correta ao entrar em linguagem sem codigo.

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,4 +2,10 @@ class Language < ActiveRecord::Base
   has_many :codes
 
   validates_presence_of :name, :slug
+
+  def self.get_name_and_slug_of(language)
+    return {:name => 'Todas', :slug => 'all'} if language == 'all'
+    language = find_by_slug(language)
+    language ? {:name => language.name, :slug => language.slug} : nil
+  end
 end
